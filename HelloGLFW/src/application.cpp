cd7baa78964570b3c691ec1ec950c8a9063e1797
Timmoth <timmoth.jones@gmail.com>
2018-02-02 19:07:24 +0000

    Edited Output & Intermediate Directorys for HelloGLFW

---
+++ HelloGLFW/src/application.cpp
@@ -0,0 +1,17 @@
+//Include iostream which contains the code needed to use cout
+#include <iostream>
+
+using namespace std;
+
+//Program entry point
+int main() {
+
+	//Output application title to the console
+	cout << "HelloGLFW" << endl;
+
+
+	//Wait until the user hits enter at the console
+	cin.get();
+	//Program returns successfully
+	return 0;
+}
