f36efc2ceff4c500fc74fdaf38a2ceb305bf884e
Timmoth <timmoth.jones@gmail.com>
2018-02-02 21:19:13 +0000

    Updated Configuration properties for HelloGLEW to include GLEW and GLFW dependencies

---
+++ 2_HelloGLEW/src/application.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+
+using namespace std;
+
+int main() {
+	cout << "Hello GLEW" << endl;
+
+	//Program returns successfully
+	return 0;
+}
