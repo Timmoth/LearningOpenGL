f77137f2b614322bb3fae085eb1a337b9dffbf33
Timmoth <timmoth.jones@gmail.com>
2018-02-03 12:15:02 +0000

    Added basic code for displaying a triangle to HelloTriangle

--- 3_HelloTriangle/src/application.cpp
+++ 3_HelloTriangle/src/application.cpp
@@ -48,8 +48,27 @@ using namespace std;
 
 int initGLFW();
 int initGLEW();
+void CreateTriangleVertexArrayObject();
+void CreateTriangleVertexBuffer();
+void RenderLoop();
 void terminate();
+
 GLFWwindow* window;
+
+//Id of the vertex array object
+unsigned int triangleVertexArrayID;
+
+//ID of the vertex buffer object;
+unsigned int triangleVertexBufferID;
+
+//Vertex buffer data - the coordinates of the points of the triangle
+//Coordinates are in screen coordinates: floats from -1.0f to 1.0f in the X,Y and Z plane
+const float triangleVertexBuffer[] = {
+	//Coordinates	  X		 Y	  Z
+				   -1.0f, -1.0f, 0.0f,	//Vertex 0
+					1.0f, -1.0f, 0.0f,	//Vertex 1
+					0.0f,  1.0f, 0.0f	//Vertex 2
+};
 
 int main() {
 
@@ -64,6 +83,10 @@ int main() {
 		return -1;
 	}
 
+	CreateTriangleVertexArrayObject();
+	CreateTriangleVertexBuffer();
+
+	RenderLoop();
 
 	terminate();
 	return 0;
@@ -97,7 +120,7 @@ int initGLEW() {
 		return 0;
 	}
 
-	cout << "Usign glew version: " << glewGetString(GLEW_VERSION) << endl;
+	cout << "Using glew version: " << glewGetString(GLEW_VERSION) << endl;
 	return 1;
 }
 
@@ -105,4 +128,53 @@ void terminate() {
 	cout << "Terminating press enter to exit" << endl;
 	cin.get();
 	glfwTerminate();
+}
+
+void CreateTriangleVertexArrayObject() {
+	//Generate a vertex array object and get it's ID in triangleVertexArrayID
+	glGenVertexArrays(1, &triangleVertexArrayID);
+	//Bind the newly created vertex array object
+	glBindVertexArray(triangleVertexArrayID);
+}
+
+void CreateTriangleVertexBuffer() {
+	//Generate a vertex buffer object and get a reference to it
+	glGenBuffers(1, &triangleVertexBufferID);
+	//Bind the Vertex Buffer object
+	glBindBuffer(GL_ARRAY_BUFFER, triangleVertexBufferID);
+	//Load the vertex buffer data into the bound vertex buffer object
+	glBufferData(
+		GL_ARRAY_BUFFER,
+		sizeof(triangleVertexBuffer), 
+		triangleVertexBuffer,
+		GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(
+		0,
+		3,
+		GL_FLOAT,
+		GL_FALSE,
+		0,
+		(void*)0
+	);
+
+}
+
+void RenderLoop() {
+	//Set glfw to capture key's pressed
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	//Render frames until the escape key is pressed
+	do {
+		
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
 }
